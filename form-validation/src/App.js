d7957585266bef48e133d196cc0033e058821b3f
Michal Bryk <galeriakoloru90210@gmail.com>
2019-12-14 00:26:08 +0100

    Add css, install font awsome package

--- form-validation/src/App.js
+++ form-validation/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css'
+import backgroundImage from './form-background-image.png'
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class App extends Component {
   state = {
@@ -18,10 +21,10 @@ class App extends Component {
     }
 
     messages = {
-      username_incorrect: 'Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji',
-      email_incorrect: 'Brak @ w emailu',
-      password_incorrect: 'Hasło musi mieć 8 znaków',
-      accept_incorrect: 'Niepotwierdzona zgoda'
+      username_incorrect: 'Your Username should be longer than 10 characters and not contains space.',
+      email_incorrect: 'Missing "@" in email.',
+      password_incorrect: 'Passsword should have 8 characters.',
+      accept_incorrect: 'Unconfirmed terms and conditions'
     }
 
     handleChange = (e) => {
@@ -123,52 +126,63 @@ class App extends Component {
 
   render() { 
     return ( 
-      <div className="App">
-        <form onSubmit={this.handleSubmit} noValidate>
-          <label htmlFor="user">Twoje imię:
+      <div style={{backgroundImage:`url(${backgroundImage})`}} className="signupSection">
+        <div className="info">
+          <h2>Classic Game Moves</h2>
+          <FontAwesomeIcon className='fa' icon={faGamepad} />
+          <p>Are you ready for journey?</p>
+        </div>
+        <form className='signupForm' onSubmit={this.handleSubmit} noValidate>
+          <h2>Sign up</h2>
+          <label htmlFor="user">
       <input 
       type="text" 
       id="user" 
       name="username" 
+      placeholder="Username"
       value={this.state.username}
       onChange={this.handleChange}
       />
    {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
           </label>
 
-          <label htmlFor="email">Twój adres e-mail:
+          <label htmlFor="email">
       <input 
       type="email" 
       id="email" 
       name="email" 
+      placeholder="Email"
       value={this.state.email}
       onChange={this.handleChange}
       />
       {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
           </label>
 
-          <label htmlFor="password">Twoje hasło:
+          <label htmlFor="password">
       <input 
       type="password" 
       id="password" 
       name="pass" 
+      placeholder="Password"
       value={this.state.pass}
       onChange={this.handleChange}
       />
       {this.state.errors.pass && <span>{this.messages.password_incorrect}</span>}
           </label>
-
           <label htmlFor="accept">
+      
             <input 
+            className='terms'
             id="accept"
             name="accept"
             type="checkbox"
             checked={this.state.accept}
             onChange={this.handleChange}
-            /> Wyrażam zgodę na przetwarzanie danych 
+            />
+            <span className="terms">I accept terms and conditions</span>
           </label>
           {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
-          <button>zapisz się</button>
+          <button>sign up</button>
         </form>
     {this.state.message && <h3>{this.state.message}</h3>}
       </div>
