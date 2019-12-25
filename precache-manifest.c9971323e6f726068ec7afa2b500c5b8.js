2c77a4adf87dc7977a076e557970cf9f83304da9
Michal Bryk <galeriakoloru90210@gmail.com>
2019-12-25 15:34:17 +0100

    Updates

---
+++ precache-manifest.c9971323e6f726068ec7afa2b500c5b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b1ae778a060944a2c91fea831847d7",
+    "url": "/FormValidation/index.html"
+  },
+  {
+    "revision": "41cef147fb4e130ca642",
+    "url": "/FormValidation/static/css/main.5821f77a.chunk.css"
+  },
+  {
+    "revision": "cb8c74fc02be51094880",
+    "url": "/FormValidation/static/js/2.7c4635a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FormValidation/static/js/2.7c4635a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41cef147fb4e130ca642",
+    "url": "/FormValidation/static/js/main.e2de7016.chunk.js"
+  },
+  {
+    "revision": "5fa3d00c87d2fb3885b8",
+    "url": "/FormValidation/static/js/runtime-main.eb12b3f4.js"
+  },
+  {
+    "revision": "9ea5cff9e3aa86b90e0a043001805d1b",
+    "url": "/FormValidation/static/media/form-background-image.9ea5cff9.png"
+  }
+]);
